3a44f9138bc1b7e6894baab2c0a7918799c5317e
Paul Ochieng Okello <paulokello273@gmail.com>
2023-06-25 17:03:10 +0300

    🔨 Refactor Code:  import formatter

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+
+import TextToSpeech from "./TextToSpeech";
+import FAQSection from "./FAQSection";
+import Features from "./Features";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+export { Navbar, Hero, Features, FAQSection, Footer,TextToSpeech };
