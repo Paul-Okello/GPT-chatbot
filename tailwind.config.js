4feb8d2d5414674ac877ec1a079c5e51f4045f5e
Paul Ochieng Okello <paulokello273@gmail.com>
2023-06-25 16:56:49 +0300

    🎉 Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // you can either add all styles
+    "./node_modules/@rewind-ui/core/dist/theme/styles/*.js",
+    // OR you can add only the styles you need
+    "./node_modules/@rewind-ui/core/dist/theme/styles/Button.styles.js",
+    "./node_modules/@rewind-ui/core/dist/theme/styles/Text.styles.js",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar")({ nocompatible: true }),
+    require("@tailwindcss/forms")({
+      strategy: "class", // only generate classes
+    }),
+  ],
+};
